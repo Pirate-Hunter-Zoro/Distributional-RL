dd36694ea6cd98e99255ee3fe42330eb2f8cb0ee
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-10-31 19:36:10 -0500

    change zrange and learning rate

--- src/C51.py
+++ src/C51.py
@@ -27,7 +27,7 @@ STEPS_MAX = 10000       # Gradually reduce epsilon over these many steps
 EPSILON_END = 0.1       # At the end, keep epsilon at this value
 MINIBATCH_SIZE = 64     # How many examples to sample per train step
 GAMMA = 0.99            # Discount factor in episodic reward objective
-LEARNING_RATE = 5e-4    # Learning rate for Adam optimizer
+LEARNING_RATE = 1e-4    # Learning rate for Adam optimizer
 TRAIN_AFTER_EPISODES = 10   # Just collect episodes for these many episodes
 TRAIN_EPOCHS = 25        # Train for these many epochs every time
 BUFSIZE = 10000         # Replay buffer size
@@ -38,7 +38,7 @@ TARGET_NETWORK_UPDATE_FREQ = 10 # Target network update frequency
 
 # Suggested constants
 ATOMS = 51              # Number of atoms for distributional network
-ZRANGE = [0, 200]       # Range for Z projection
+ZRANGE = [0, 500]       # Range for Z projection
 z = torch.linspace(ZRANGE[0], ZRANGE[1], ATOMS).to(DEVICE)
 delta_z = (ZRANGE[1] - ZRANGE[0]) / (ATOMS - 1)
 
